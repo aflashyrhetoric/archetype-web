a844cc44d380ad6668cb13f90141c62fe5ede8b5
Kevin Oh <aflashyrhetoric@gmail.com>
2022-05-08 23:10:32 -0400

    Load stuff from the backend officially

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,32 @@
 import React from "react"
-import Articles from "../components/articles"
+import ReactMarkdown from "react-markdown"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { fetchAPI } from "../lib/api"
 
-const Home = ({ products, homepage }) => {
+const Home = ({ hero, seo, explore_archetypes }) => {
+  const { heading, subheading, kits } = explore_archetypes
+  const { data: kitsList } = kits
+
   return (
     <Layout categories={[]}>
-      <Seo seo={homepage.attributes.seo} />
+      <Seo seo={seo} />
       <div className="uk-section">
         <div className="uk-container uk-container-large">
-          <h1>{homepage.attributes.hero.title}</h1>
-          {/* <code>
-            <pre>{JSON.stringify(products[0])}</pre>
-          </code> */}
-          <Articles products={products} />
+          <h1>{hero.title}</h1>
+
+          <h2>{heading}</h2>
+          <p>{subheading}</p>
+          {kitsList.map((kit) => {
+            const { name, card_summary, main_description } = kit.attributes
+            return (
+              <div key={name}>
+                <h2>{name}</h2>
+                <ReactMarkdown source={card_summary} escapeHtml={false} />
+                <ReactMarkdown source={main_description} escapeHtml={false} />
+              </div>
+            )
+          })}
         </div>
       </div>
     </Layout>
@@ -23,20 +35,23 @@ const Home = ({ products, homepage }) => {
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const [productsRes, homepageRes] = await Promise.all([
-    fetchAPI("/products", { populate: "*" }),
+  const [homepageRes] = await Promise.all([
     fetchAPI("/homepage", {
       populate: {
         hero: "*",
         seo: { populate: "*" },
+        explore_archetypes: { populate: "*" },
       },
     }),
   ])
 
+  const { hero, seo, explore_archetypes } = homepageRes?.data.attributes
+
   return {
     props: {
-      products: productsRes.data,
-      homepage: homepageRes.data,
+      hero,
+      seo,
+      explore_archetypes,
     },
     revalidate: 1,
   }
